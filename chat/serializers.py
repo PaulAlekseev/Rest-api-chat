d9bde2b3478ef63723dcdbc0e2d6b1dc68d2653d
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-08-25 21:15:18 +0300

    created serializers for Message and Topic models

---
+++ chat/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from chat.models import Message, Topic
+
+
+class MessageSerializer(ModelSerializer):
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+
+class TopicSerializer(ModelSerializer):
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
+    class Meta:
+        model = Topic
+        fields = '__all__'
