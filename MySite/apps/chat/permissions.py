74df4ae54f0d0a13ebb42bb08d47ead157e0796a
dante <pavel.zommerfeld@yandex.ru>
2022-09-02 19:32:31 +0300

    Permissions bug fixed.

--- MySite/apps/chat/permissions.py
+++ MySite/apps/chat/permissions.py
@@ -2,8 +2,13 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
 
         return obj.owner == request.user
